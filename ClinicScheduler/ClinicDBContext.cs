7e49a30e1da110da5f6794bc6410d7a4c2d73460
BlazejKalkowski <blazej.kolek@gmail.com>
2024-03-13 22:39:06 +0100

    Added DBContext class.

---
+++ ClinicScheduler/ClinicDBContext.cs
@@ -0,0 +1,11 @@
+using ClinicScheduler.Entieties;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClinicScheduler;
+
+public class ClinicDbContext(DbContextOptions<ClinicDbContext> options) : DbContext(options)
+{
+    public DbSet<Doctor> Doctors { get; set; }
+    public DbSet<Patient> Patients { get; set; }
+    public DbSet<Visit> Visits { get; set; }
+}
