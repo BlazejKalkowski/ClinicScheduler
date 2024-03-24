643a785a68ce7e559bb53dc125c06992e686c002
BlazejKalkowski <blazej.kolek@gmail.com>
2024-03-24 20:42:49 +0100

    Update doctor secvice and add database handle.

--- ClinicScheduler/Services/DoctorService.cs
+++ ClinicScheduler/Services/DoctorService.cs
@@ -1,56 +1,51 @@
 ﻿using ClinicScheduler.Entities;
 using ClinicScheduler.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace ClinicScheduler.Services
 {
     public class DoctorService : IDoctorService
     {
-        public static List<Doctor> _doctors = new List<Doctor> 
+        private readonly ClinicDbContext _dbContext;
+        
+        public DoctorService(ClinicDbContext dbContext)
         {
-            new Doctor(){Id = 1, Name = "Artur", LastName = "Jablonski", Specialization = "Cardiologist"},
-            new Doctor(){Id = 2, Name = "Jerzy", LastName = "Mineralski", Specialization = "Dermatologist"},
-            new Doctor(){Id = 3, Name = "Natalia", LastName = "Almanska", Specialization = "Pediatrician"},
-        };
-
-        public async Task<List<Doctor>> GetAllDoctorsAsync() 
-        {
-            await Task.Delay(100);
-            return _doctors;
+            _dbContext = dbContext;
         }
 
-
+        public async Task<List<Doctor>> GetAllDoctorsAsync() => await _dbContext.Doctors.ToListAsync();
+        
         public async Task<Doctor> GetDoctorByIdAsync(int id)
         {
-            await Task.Delay(100);
-            var doctor =  _doctors.Where(x => x.Id == id).FirstOrDefault();
+            var doctor = await _dbContext.Doctors.Where(x => x.Id == id).FirstOrDefaultAsync();
             return doctor;
         }
 
         public async Task AddDoctorAsync(Doctor doctor)
         {
-            await Task.Delay(100);
             _doctors.Add(doctor);
+            await _dbContext.SaveChangesAsync();
         }
 
         public async Task UpdateDoctorAsync(Doctor doctor, int id)
         {
-            await Task.Delay(100);
-            var dbDoctor = _doctors.Where(x => x.Id == id).FirstOrDefault();
+            var dbDoctor = await _dbContext.Doctors.Where(x => x.Id == id).FirstOrDefaultAsync();
             if (dbDoctor != null)
             {
                 dbDoctor.Name = doctor.Name;
                 dbDoctor.LastName = doctor.LastName;
                 dbDoctor.Specialization = doctor.Specialization;
             }
+            await _dbContext.SaveChangesAsync();
         }
 
         public async Task DeleteDoctorAsync(int id)
         {
-            await Task.Delay(100);
-            var dbDoctor = _doctors.Where(x => x.Id == id).FirstOrDefault();
+            var dbDoctor = await _dbContext.Doctors.Where(x => x.Id == id).FirstOrDefaultAsync();
             if (dbDoctor != null)
             {
-                _doctors.Remove(dbDoctor);
+                _dbContext.Remove(dbDoctor);
+                await _dbContext.SaveChangesAsync();
             }
         }
     }
