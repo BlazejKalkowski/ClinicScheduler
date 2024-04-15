b07bb764d33f39d776854978f3be987a5644cc50
BlazejKalkowski <blazej.kolek@gmail.com>
2024-04-15 10:35:54 +0200

    Added prerendering to login page.

--- ClinicScheduler/Program.cs
+++ ClinicScheduler/Program.cs
@@ -69,9 +69,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-app.UseAuthentication();
 app.UseAuthorization();
-
+app.UseAuthentication();
 app.UseAntiforgery();
 app.MapRazorPages();
 app.MapIdentityApi<IdentityUser>();
